b3480d77f98623c93f74aeb4d6ca08e8d7561083
Sky <github@codestorm.net>
2025-08-17 17:47:51 +0200

    Call documentation "documentation" and remove unnecessary links in the footer

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -90,7 +90,7 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Documentation',
         },
 //        {to: '/blog', label: 'Blog', position: 'left'},
         {
@@ -103,32 +103,6 @@ const config: Config = {
     footer: {
       style: 'dark',
       links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'X',
-              href: 'https://x.com/docusaurus',
-            },
-          ],
-        },
 //        {
 //          title: 'More',
 //          items: [
