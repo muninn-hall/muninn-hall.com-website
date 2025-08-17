2f686264a5b862ac43bd9ecd85279822e48b7257
Sky <github@codestorm.net>
2025-08-17 19:10:06 +0200

    put footer back

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -103,6 +103,32 @@ const config: Config = {
     footer: {
       style: 'dark',
       links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'X',
+              href: 'https://x.com/docusaurus',
+            },
+          ],
+        },
 //        {
 //          title: 'More',
 //          items: [
