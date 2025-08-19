31ca54392c686cadf492fc9ae6e73a01c78376dd
Catalan Lover <catalanlover@protonmail.com>
2025-08-19 17:19:13 +0200

    Publish 2025-08-19/introducing-muninn-hall

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -93,7 +93,7 @@ const config: Config = {
           position: 'left',
           label: 'Documentation',
         },
-//        {to: '/blog', label: 'Blog', position: 'left'},
+        {to: '/blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/muninn-hall/muninn-hall.com-website/',
           label: 'GitHub',
@@ -129,10 +129,10 @@ const config: Config = {
         {
           title: 'More',
           items: [
-//            {
-//              label: 'Blog',
-//              to: '/blog',
-//            },
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
             {
               label: 'GitHub',
              href: 'https://github.com/muninn-hall/muninn-hall.com-website/',
